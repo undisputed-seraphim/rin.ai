1fa7afffd9ab7954e6a025dac8ca9ead0da540e7
Fluttershy <ecchisushi@gmail.com>
2012-09-21 20:37:36 -0400

    ...no comment

--- inc/js/webgl.js
+++ inc/js/webgl.js
@@ -5,6 +5,6 @@ body.onload = function() {
 
 function goAhead() {
 	//$r.scene.add( "obj", { name: "ultimecia", range: 33 } );
-	$r.scene.add( "obj", { name: "uni" } );
+	$r.scene.add( "obj", { name: "uni", range: 5, rate: 100 } );
 	$r.start();
 }
