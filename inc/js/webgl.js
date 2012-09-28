91ea865c7a9d5e210eb5f800d6ffd34055026d18
Fluttershy <ecchisushi@gmail.com>
2012-09-28 00:31:16 -0400

    ...no comment

--- inc/js/webgl.js
+++ inc/js/webgl.js
@@ -8,7 +8,7 @@ function goAhead() {
 		amap: { "idle": [1, 33], "hit1": [34, 52], "hit2": [53, 75],
 		"attack1": [76, 128], "attack2": [129, 169], "next": [170, 250] } } );*/
 	$r.scene.add( "obj", { name: "blackheart_v", type: "character", position: [-1,1,0] } );
-	$r.scene.add( "obj", { name: "miku", type: "caracter" } );
+	$r.scene.add( "obj", { name: "purplesister_mk2", type: "caracter" } );
 	//var p = new $r.$Primitive( "cube" );
 	//$r.scene.add( p );
 	$r.start();
